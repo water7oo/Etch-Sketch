2331e9c9243a73bed732b3238ff87bda150d31e7
water7oo <watersevens37@gmail.com>
2023-07-05 11:45:51 -0400

    Added canvas size changes

--- EtchSketch.js
+++ EtchSketch.js
@@ -1,28 +1,45 @@
 // Get the container element
 const container = document.getElementById("grid-container");
+let gridRowCount = 20;
+let gridColumnCount = 20;
+
+// Calculate the square size based on the grid size and container dimensions
+const containerWidth = container.offsetWidth;
+const containerHeight = container.offsetHeight;
+const minSquareSize = 20; // Minimum size of the grid square
+
+// Calculate the maximum square size based on the container dimensions and the number of rows and columns
+const maxSquareSize = Math.floor(
+  Math.min(containerWidth / gridColumnCount, containerHeight / gridRowCount)
+);
+
+// Determine the actual square size based on the maximum and minimum sizes
+const squareSize = Math.max(minSquareSize, maxSquareSize);
 
 // Create a loop to generate rows
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < gridRowCount; i++) {
   // Create a new row element
   const row = document.createElement("div");
   row.classList.add("row"); // Optional: Add a CSS class to the row element
 
-  // Create a loop to generate columns within the row
-  for (let j = 0; j < 16; j++) {
-    // Create a new column element
+  for (let j = 0; j < gridColumnCount; j++) {
     const column = document.createElement("div");
     column.classList.add("column"); // Optional: Add a CSS class to the column element
+
+    // Set the size of the columns based on the calculated square size
+    column.style.width = `${squareSize}px`;
+    column.style.height = `${squareSize}px`;
 
     var isMouseDown = false;
     // Add event listeners for mouse actions
     column.addEventListener("mousedown", () => {
       isMouseDown = true;
-      column.style.backgroundColor = "black"; // Change the background color when mouse button is pressed
+      column.style.backgroundColor = "black"; // Change the background color when the mouse button is pressed
     });
 
     column.addEventListener("mouseover", () => {
       if (isMouseDown) {
-        column.style.backgroundColor = "black"; // Change the background color on hover while mouse button is pressed
+        column.style.backgroundColor = "black"; // Change the background color on hover while the mouse button is pressed
       }
     });
 
@@ -38,6 +55,11 @@ for (let i = 0; i < 16; i++) {
   container.appendChild(row);
 }
 
-function newGrid() {
-  console.log("The button is working");
-}
+function newGrid() {}
+
+//Add undo and redo
+//Add eraser
+//Add colors
+//Fix grid size issue
+//User brush size option
+//User can save their work as png,jpeg, etc.
